e419219ef55b3fa022308046fd14940bba63836d
fdaryn-h <syndima43@yandex.ru>
2019-04-13 01:02:08 +0300

    added flags

--- ft_printf.h
+++ ft_printf.h
@@ -1,6 +1,19 @@
-#ifndef PRINTF_FT_PRINTF_H
-#define PRINTF_FT_PRINTF_H
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
 
+typedef	struct	s_f
+{
+	int	plu;
+	int	min;
+	int	zer;
+	int spc;
+	int okt;
+	int h;
+	int hh;
+	int l;
+	int ll;
+	int bl;
+}				t_f;
 int	ft_printf(const char *format, ...);
 
-#endif //PRINTF_FT_PRINTF_H
+#endif
