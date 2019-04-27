8fa4cfa9164bb5d0af8e6b4921c5085b9483033b
fdaryn-h <syndima43@yandex.ru>
2019-04-27 20:31:43 +0300

    added tests

---
+++ int_test.c
@@ -0,0 +1,58 @@
+#include "ft_printf.h"
+#include <stdio.h>
+
+void	int_test(void)
+{
+	printf("% 20.10i\n", -100);
+	ft_printf("% 20.10i\n", -100);
+	printf("% 20.10i\n", 100);
+	ft_printf("% 20.10i\n", 100);
+	printf("% d\n", 123125);
+	ft_printf("% d\n", 123125);
+	printf("%+d\n", 123125);
+	ft_printf("%+d\n", 123125);
+	printf("%010d\n", 123125);
+	ft_printf("%010d\n", 123125);
+	printf("%-10d\n", 123125);
+	ft_printf("%-10d\n", 123125);
+	printf("%-+10d\n", 123125);
+	ft_printf("%-+10d\n", 123125);
+	printf("% -10d\n", 123125);
+	ft_printf("% -10d\n", 123125);
+	printf("% 010d\n", 123125);
+	ft_printf("% 010d\n", 123125);
+	printf("%3.2d\n", 0);
+	ft_printf("%3.2d\n", 0);
+	printf("% d\n", 12);
+	ft_printf("% d\n", 12);
+	printf("%+d\n", 12);
+	ft_printf("%+d\n", 12);
+	printf("%010d\n", 12);
+	ft_printf("%010d\n", 12);
+	printf("%-10d\n", 12);
+	ft_printf("%-10d\n", 12);
+	printf("%-+10d\n", 12);
+	ft_printf("%-+10d\n", 12);
+	printf("% -10d\n", 12);
+	ft_printf("% -10d\n", 12);
+	printf("% 010d\n", 12);
+	ft_printf("% 010d\n", 12);
+	printf("%+010d\n", 12);
+	ft_printf("%+010d\n", 12);
+	printf("% d\n", -123125);
+	ft_printf("% d\n", -123125);
+	printf("%+d\n", -123125);
+	ft_printf("%+d\n", -123125);
+	printf("%010d\n", -123125);
+	ft_printf("%010d\n", -123125);
+	printf("%-10d\n", -123125);
+	ft_printf("%-10d\n", -123125);
+	printf("%-+10d\n", -123125);
+	ft_printf("%-+10d\n", -123125);
+	printf("% -10d\n", -123125);
+	ft_printf("% -10d\n", -123125);
+	printf("% 010d\n", -123125);
+	ft_printf("% 010d\n", -123125);
+	printf("%+010d\n", -123125);
+	ft_printf("%+010d\n", -123125);
+}
---
+++ tests.h
@@ -0,0 +1,6 @@
+#ifndef PRINTF_TESTS_H
+#define PRINTF_TESTS_H
+
+void	int_test(void);
+
+#endif //PRINTF_TESTS_H
